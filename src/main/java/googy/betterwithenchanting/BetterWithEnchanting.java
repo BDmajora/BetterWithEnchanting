6f27766ebb83784130edf5abb183dda311837bc6
bdmajora <bdmajora@protonmail.com>
2024-03-25 21:57:45 -0600

    I don't know what I messed up

--- src/main/java/googy/betterwithenchanting/BetterWithEnchanting.java
+++ src/main/java/googy/betterwithenchanting/BetterWithEnchanting.java
@@ -5,7 +5,7 @@ import googy.betterwithenchanting.block.entity.TileEntityEnchantmentTable;
 import googy.betterwithenchanting.mixin.PacketMixin;
 import googy.betterwithenchanting.network.packet.PacketEnchantItem;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.client.sound.block.BlockSound;
+import net.minecraft.core.sound.BlockSounds;
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.tag.BlockTags;
 import net.minecraft.core.item.Item;
@@ -17,18 +17,19 @@ import turniplabs.halplibe.helper.EntityHelper;
 import turniplabs.halplibe.helper.NetworkHelper;
 import turniplabs.halplibe.helper.RecipeHelper;
 import turniplabs.halplibe.util.ConfigHandler;
+import turniplabs.halplibe.util.RecipeEntrypoint;
 
 import java.util.Properties;
 
 
-public class BetterWithEnchanting implements ModInitializer {
+public class BetterWithEnchanting implements ModInitializer, RecipeEntrypoint {
     public static final String MOD_ID = "betterwithenchanting";
     public static final Logger LOG = LoggerFactory.getLogger(MOD_ID);
 
 
 
 	public static final Block enchantmentTable = new BlockBuilder(MOD_ID)
-		.setBlockSound(new BlockSound("step.stone", "step.stone", 1.0f, 1.0f))
+		.setBlockSound(BlockSounds.STONE)
 		.setHardness(5)
 		.setResistance(1200)
 		.setLuminance(7)
@@ -37,20 +38,6 @@ public class BetterWithEnchanting implements ModInitializer {
 		.setSideTextures("enchantment_table/side.png")
 		.setTopTexture("enchantment_table/top.png")
 		.build(new BlockEnchantmentTable("enchantmenttable", Global.config.getInt("enchantment_table_id")));
-
-
-	static {
-		EntityHelper.createTileEntity(TileEntityEnchantmentTable.class, "EnchantmentTable");
-
-		RecipeHelper.Crafting.createRecipe(new ItemStack(enchantmentTable), new Object[]{
-			" B ",
-			"DCD",
-			"CCC",
-			'B', Item.book,
-			'C', Block.cobbleStone,
-			'D', Global.config.getBoolean("expensive_crafting") ? Block.blockDiamond : Item.diamond
-		});
-	}
 
 	public BetterWithEnchanting()
 	{
@@ -66,4 +53,19 @@ public class BetterWithEnchanting implements ModInitializer {
 
 
     }
+
+	public void onRecipesReady() {
+		EntityHelper.Core.createTileEntity(TileEntityEnchantmentTable.class, "EnchantmentTable");
+
+		RecipeHelper.Crafting.createRecipe(new ItemStack(enchantmentTable), new Object[]{
+			" B ",
+			"DCD",
+			"CCC",
+			'B', Item.book,
+			'C', Block.cobbleStone,
+			'D', Global.config.getBoolean("expensive_crafting") ? Block.blockDiamond : Item.diamond
+		});
+	}
+
+
 }
