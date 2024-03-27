27b1c71bedd4911679ac4e9c43892cdccf06c319
Turing6 <39919441+Testure@users.noreply.github.com>
2024-03-27 18:17:36 -0500

    Add book model + fixes

--- src/main/java/googy/betterwithenchanting/BetterWithEnchanting.java
+++ src/main/java/googy/betterwithenchanting/BetterWithEnchanting.java
@@ -1,32 +1,27 @@
 package googy.betterwithenchanting;
 
 import googy.betterwithenchanting.block.BlockEnchantmentTable;
+import googy.betterwithenchanting.block.client.EnchantmentTableRenderer;
 import googy.betterwithenchanting.block.entity.TileEntityEnchantmentTable;
-import googy.betterwithenchanting.mixin.PacketMixin;
+import googy.betterwithenchanting.mixin.TileEntityRenderDispatcherAccessor;
 import googy.betterwithenchanting.network.packet.PacketEnchantItem;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.core.sound.BlockSounds;
+import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+import net.minecraft.client.render.TileEntityRenderDispatcher;
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.tag.BlockTags;
 import net.minecraft.core.item.Item;
-import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.sound.BlockSounds;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import turniplabs.halplibe.helper.BlockBuilder;
-import turniplabs.halplibe.helper.EntityHelper;
-import turniplabs.halplibe.helper.NetworkHelper;
-import turniplabs.halplibe.helper.RecipeHelper;
-import turniplabs.halplibe.util.ConfigHandler;
+import turniplabs.halplibe.helper.*;
+import turniplabs.halplibe.util.ClientStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
-import java.util.Properties;
 
-
-public class BetterWithEnchanting implements ModInitializer, RecipeEntrypoint {
+public class BetterWithEnchanting implements ModInitializer, RecipeEntrypoint, PreLaunchEntrypoint, ClientStartEntrypoint {
     public static final String MOD_ID = "betterwithenchanting";
     public static final Logger LOG = LoggerFactory.getLogger(MOD_ID);
-
-
 
 	public static final Block enchantmentTable = new BlockBuilder(MOD_ID)
 		.setBlockSound(BlockSounds.STONE)
@@ -39,33 +34,41 @@ public class BetterWithEnchanting implements ModInitializer, RecipeEntrypoint {
 		.setTopTexture("enchantment_table/top.png")
 		.build(new BlockEnchantmentTable("enchantmenttable", Global.config.getInt("enchantment_table_id")));
 
-	public BetterWithEnchanting()
-	{
-		PacketMixin.callAddIdClassMapping(Global.config.getInt("packet_enchant_id"), false, true, PacketEnchantItem.class);
-	}
-
-
     @Override
     public void onInitialize() {
 
 		LOG.info("BetterWithEnchanting initialized!");
 
-
-
     }
+
+	@Override
+	public void beforeClientStart() {
+		EnchantmentTableRenderer renderer = new EnchantmentTableRenderer();
+		((TileEntityRenderDispatcherAccessor) TileEntityRenderDispatcher.instance).getRenderers().put(TileEntityEnchantmentTable.class, renderer);
+		renderer.setRenderDispatcher(TileEntityRenderDispatcher.instance);
+	}
+
+	@Override
+	public void afterClientStart() {
+
+	}
+
+	@Override
+	public void onPreLaunch() {
+		NetworkHelper.register(PacketEnchantItem.class, true, false);
+	}
 
 	public void onRecipesReady() {
 		EntityHelper.Core.createTileEntity(TileEntityEnchantmentTable.class, "EnchantmentTable");
 
-		RecipeHelper.Crafting.createRecipe(new ItemStack(enchantmentTable), new Object[]{
+		RecipeBuilder.Shaped(MOD_ID,
 			" B ",
 			"DCD",
-			"CCC",
-			'B', Item.book,
-			'C', Block.cobbleStone,
-			'D', Global.config.getBoolean("expensive_crafting") ? Block.blockDiamond : Item.diamond
-		});
+			"CCC"
+		)
+			.addInput('B', Item.book)
+			.addInput('C', "minecraft:cobblestones")
+			.addInput('D', Global.config.getBoolean("expensive_crafting") ? Block.blockDiamond : Item.diamond)
+			.create("enchantment_table", enchantmentTable.getDefaultStack());
 	}
-
-
 }
