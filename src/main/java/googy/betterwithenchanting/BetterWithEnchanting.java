c79f5ad2afa6d42df236bcd6fa5eff604aec1cee
Turing6 <39919441+Testure@users.noreply.github.com>
2024-04-16 00:55:22 -0500

    Proper tile entity creation

--- src/main/java/googy/betterwithenchanting/BetterWithEnchanting.java
+++ src/main/java/googy/betterwithenchanting/BetterWithEnchanting.java
@@ -3,11 +3,9 @@ package googy.betterwithenchanting;
 import googy.betterwithenchanting.block.BlockEnchantmentTable;
 import googy.betterwithenchanting.block.client.EnchantmentTableRenderer;
 import googy.betterwithenchanting.block.entity.TileEntityEnchantmentTable;
-import googy.betterwithenchanting.mixin.TileEntityRenderDispatcherAccessor;
 import googy.betterwithenchanting.network.packet.PacketEnchantItem;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
-import net.minecraft.client.render.TileEntityRenderDispatcher;
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.tag.BlockTags;
 import net.minecraft.core.item.Item;
@@ -16,10 +14,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import turniplabs.halplibe.helper.*;
 import turniplabs.halplibe.util.ClientStartEntrypoint;
+import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
 
-public class BetterWithEnchanting implements ModInitializer, RecipeEntrypoint, PreLaunchEntrypoint, ClientStartEntrypoint {
+public class BetterWithEnchanting implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint, PreLaunchEntrypoint, ClientStartEntrypoint {
     public static final String MOD_ID = "betterwithenchanting";
     public static final Logger LOG = LoggerFactory.getLogger(MOD_ID);
 
@@ -42,10 +41,18 @@ public class BetterWithEnchanting implements ModInitializer, RecipeEntrypoint, P
     }
 
 	@Override
+	public void beforeGameStart() {
+		EntityHelper.Core.createTileEntity(TileEntityEnchantmentTable.class, "EnchantmentTable");
+	}
+
+	@Override
+	public void afterGameStart() {
+
+	}
+
+	@Override
 	public void beforeClientStart() {
-		EnchantmentTableRenderer renderer = new EnchantmentTableRenderer();
-		((TileEntityRenderDispatcherAccessor) TileEntityRenderDispatcher.instance).getRenderers().put(TileEntityEnchantmentTable.class, renderer);
-		renderer.setRenderDispatcher(TileEntityRenderDispatcher.instance);
+		EntityHelper.Client.assignTileEntityRenderer(TileEntityEnchantmentTable.class, new EnchantmentTableRenderer());
 	}
 
 	@Override
@@ -59,8 +66,6 @@ public class BetterWithEnchanting implements ModInitializer, RecipeEntrypoint, P
 	}
 
 	public void onRecipesReady() {
-		EntityHelper.Core.createTileEntity(TileEntityEnchantmentTable.class, "EnchantmentTable");
-
 		RecipeBuilder.Shaped(MOD_ID,
 			" B ",
 			"DCD",
